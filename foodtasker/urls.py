7cf6695240160efe007d06111227a635c2e7789e
Bekhzod <kiral439@yandex.ru>
2020-07-06 10:18:04 +0500

    Deploying

--- foodtasker/urls.py
+++ foodtasker/urls.py
@@ -15,7 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from foodtaskerapp import views
+from django.contrib.auth import views as auth_view
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', views.home, name='home'),
+    path('restaurant/sign-in/', auth_view.LoginView.as_view(template_name='restaurant/sign_in.html'), name = 'restaurant-sign-in'),
+    # path('restaurant/sign-out', auth_view.LogoutView.as_view(next_page=settings.LOGIN_REDIRECT_URL), name = 'restaurant-sign-out'),
+    path('restaurant/sign-out/', auth_view.LogoutView.as_view(next_page='/'), name = 'restaurant-sign-out'),
+    path('restaurant/', views.restaurant_home, name='restaurant-home'),
+    path('restaurant/sign-up/', views.restaurant_sign_up, name = 'restaurant-sign-up'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
