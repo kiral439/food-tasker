9d5de677c050db2dcb2cfe3e432df9dbc9b8b843
Bekhzod Turdialiev <bekhzodturdialiev@gmail.com>
2020-07-07 10:24:33 +0500

    fix

--- foodtasker/settings.py
+++ foodtasker/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_HOST = os.environ.get('DJANGO_STATIC_HOST', '')
-STATIC_URL = STATIC_HOST + '/static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
