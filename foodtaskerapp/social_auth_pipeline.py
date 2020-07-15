66018242c4af7dbd416d547dbeb883797ec4fd14
Bekhzod Turdialiev <bekhzodturdialiev@gmail.com>
2020-07-15 11:33:58 +0500

    Social auth pipeline <user_type> fix

--- foodtaskerapp/social_auth_pipeline.py
+++ foodtaskerapp/social_auth_pipeline.py
@@ -1,13 +1,16 @@
 from .models import Customer, Driver
 
-def create_user_by_type(backend, user, response, *args, **kwargs):
-    request = backend.strategy.request_data()
+
+def create_user_by_type(backend, strategy, user,
+                        response, *args, **kwargs):
+    request = strategy.request
     if backend.name == 'facebook':
         avatar = 'https://graph.facebook.com/%s/picture?type=large' % response['id']
 
-    if request["user_type"] == "driver" and not Driver.objects.filter(user_id=user.id):
-        Driver.objects.create(user_id=user.id, avatar = avatar)
-    elif request["user_type"] == "customer" and not Customer.objects.filter(user_id=user.id):
-        Customer.objects.create(user_id=user.id, avatar = avatar)
+    if request.POST.get("user_type") == "driver" and not Driver.objects.filter(
+            user_id=user.id):
+        Driver.objects.create(user_id=user.id, avatar=avatar)
+    elif request.POST.get("user_type") == "customer" and not Customer.objects.filter(user_id=user.id):
+        Customer.objects.create(user_id=user.id, avatar=avatar)
     else:
         pass
