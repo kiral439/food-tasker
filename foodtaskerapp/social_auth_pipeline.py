a96f3a6a53eae0ab2ceb0d98503c44b34065a81f
Bekhzod Turdialiev <bekhzodturdialiev@gmail.com>
2020-07-15 15:55:57 +0500

    Responsive table

--- foodtaskerapp/social_auth_pipeline.py
+++ foodtaskerapp/social_auth_pipeline.py
@@ -7,10 +7,10 @@ def create_user_by_type(backend, strategy, user,
     if backend.name == 'facebook':
         avatar = 'https://graph.facebook.com/%s/picture?type=large' % response['id']
 
-    if request.POST.get("user_type") == "driver" and not Driver.objects.filter(
+    if request.POST.get('user_type') == "driver" and not Driver.objects.filter(
             user_id=user.id):
         Driver.objects.create(user_id=user.id, avatar=avatar)
-    elif request.POST.get("user_type") == "customer" and not Customer.objects.filter(user_id=user.id):
+    elif request.POST.get('user_type') == "customer" and not Customer.objects.filter(user_id=user.id):
         Customer.objects.create(user_id=user.id, avatar=avatar)
     else:
         pass
